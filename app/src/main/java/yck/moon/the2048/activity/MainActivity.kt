56ddd0ad48a8ee189e074fff2f9b485527c81f54
85chong <yckpc@yckPCui-MacBookPro.local>
2023-11-09 09:57:15 +0900

    * Refactor(작업)    - 랜덤값 생성 작업

--- app/src/main/java/yck/moon/the2048/activity/MainActivity.kt
+++ app/src/main/java/yck/moon/the2048/activity/MainActivity.kt
@@ -1,30 +1,50 @@
 package yck.moon.the2048.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.gson.Gson
 import timber.log.Timber
 import yck.moon.the2048.R
-import yck.moon.the2048.activity.adapter.ImageAdapter
 import yck.moon.the2048.activity.utils.DefUtil
 import yck.moon.the2048.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
-    var mainBinding: ActivityMainBinding? = null
-    var mArrList: ArrayList<String> = arrayListOf()
 
+
+    var mainBinding: ActivityMainBinding? = null
+    var mNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        var ranNums = DefUtil.getRandomNumForKeyPad(9)
-        for (number in ranNums) {
-            mArrList.add(number.toString())
-        }
-        mainBinding?.gridView?.adapter = ImageAdapter(this, mArrList)
+        // 현재 binding시킨 xml의 variable name
+        initKeypadRandomNum()
     }
 
+    fun initKeypadRandomNum() {
+        try {
+            var randomPadNumArrList = ArrayList<Int>()
+            randomPadNumArrList.addAll(DefUtil.getRandomNumForKeyPad(mNumber))
+            for (i in 0 until mNumber) {
+                val resource_id: Int = resources.getIdentifier("tv_$i", "id", packageName)
+                val tv: TextView = findViewById(resource_id)
+                if (i == randomPadNumArrList.size) {
+                    tv.text = ""
+                    return
+                }
+                if (randomPadNumArrList[i].toString().isNotEmpty()){
+                    tv.text = ""
+                } else {
+                    tv.text = randomPadNumArrList[i].toString()
+                }
+            }
+        } catch(e: Exception) {
+            Timber.tag("YCK_LOG").e("${e.message}")
+        }
+    }
 }
--- app/src/main/java/yck/moon/the2048/activity/utils/DefUtil.kt
+++ app/src/main/java/yck/moon/the2048/activity/utils/DefUtil.kt
@@ -2,12 +2,25 @@ package yck.moon.the2048.activity.utils
 
 object DefUtil {
 
+    var mRanNum = arrayListOf(2,4,8,16)
+
+    /* 랜덤 숫자 값 추출 */
+//    fun getRandomNumForKeyPad(targetValue: Int): Set<Int> {
+//        var randomPadNum = mutableSetOf<Int>()
+//        while (randomPadNum.size < targetValue) {
+//            randomPadNum.add((0 until  targetValue).random())
+//        }
+//        return randomPadNum
+//    }
+
     /* 랜덤 숫자 값 추출 */
     fun getRandomNumForKeyPad(targetValue: Int): Set<Int> {
         var randomPadNum = mutableSetOf<Int>()
+
         while (randomPadNum.size < targetValue) {
-            randomPadNum.add((0 until  9).random())
+            randomPadNum.add((0 until  mRanNum.size).random())
         }
+
         return randomPadNum
     }
 
